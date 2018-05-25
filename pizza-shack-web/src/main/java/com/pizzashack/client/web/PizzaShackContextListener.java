380f78ce6cde474a491d6df0b479374b40f286f1
ripo <ripo@nologin.es>
2018-05-25 20:07:55 +0200

    web: Fetch-configuration-from-environment

--- pizza-shack-web/src/main/java/com/pizzashack/client/web/PizzaShackContextListener.java
+++ pizza-shack-web/src/main/java/com/pizzashack/client/web/PizzaShackContextListener.java
@@ -7,23 +7,35 @@ public class PizzaShackContextListener implements ServletContextListener {
 
 	@Override
 	public void contextDestroyed(ServletContextEvent event) {
-		
+
 	}
 
 	@Override
-	public void contextInitialized(ServletContextEvent event) {		
-		String serverURL = event.getServletContext().getInitParameter("serverURL");
+	public void contextInitialized(ServletContextEvent event) {
+		String serverURL = System.getenv("API_SERVER_URL");
+                if (serverURL == null || serverURL.isEmpty()) {
+                    serverURL = event.getServletContext().getInitParameter("serverURL");
+                }
 		System.out.println("Setting server URL to: " + serverURL);
 		PizzaShackWebConfiguration.getInstance().setServerURL(serverURL);
-		
-		String loginURL = event.getServletContext().getInitParameter("loginURL");
+
+		String loginURL = System.getenv("API_LOGIN_URL");
+                if (loginURL == null || loginURL.isEmpty()) {
+                    loginURL = event.getServletContext().getInitParameter("loginURL");
+                }
         //String loginURL = "https://10.150.3.80:9443/_WSO2AMLoginAPI_";
         PizzaShackWebConfiguration.getInstance().setLoginURL(loginURL);
-		
-		String consumerKey = event.getServletContext().getInitParameter("consumerKey");
+
+		String consumerKey = System.getenv("API_CONSUMER_KEY");
+                if (consumerKey == null || consumerKey.isEmpty()) {
+                    event.getServletContext().getInitParameter("consumerKey");
+                }
 		PizzaShackWebConfiguration.getInstance().setConsumerKey(consumerKey);
-		
-		String consumerSecret = event.getServletContext().getInitParameter("consumerSecret");
+
+		String consumerSecret = System.getenv("API_CONSUMER_SECRET");
+                if (consumerSecret == null || consumerSecret.isEmpty()) {
+                    event.getServletContext().getInitParameter("consumerSecret");
+                }
 		PizzaShackWebConfiguration.getInstance().setConsumerSecret(consumerSecret);
 	}
 

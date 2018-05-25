6f81bbfaef1a510d6d3f155094a74812be0c7ac1
ripo <ripo@nologin.es>
2018-05-25 19:48:34 +0200

    Remove spring dependencies and use Jersey instead

---
+++ pizza-shack-api/src/main/java/com/pizzashack/PizzaShackAPI.java
@@ -0,0 +1,13 @@
+package com.pizzashack;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author ripo
+ */
+@ApplicationPath("/api")
+public class PizzaShackAPI extends Application {
+
+}
